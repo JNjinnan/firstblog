0049734d5f054a87243d54f68cb6ab53ceef9004
金楠 <1299973527@qq.com>
2021-08-12 19:46:28 +0800

    '完善了galleryAPP'

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,10 @@ from django.db import models
 # 当创建了新的model就要做迁移文件
 
 class Gallery(models.Model):
-    description=models.CharField(max_length=100)  # 创建结束后，要在admin中进行注册
+    description=models.CharField(default='在这里写作品的简介',max_length=100)  # 创建结束后，要在admin中进行注册
+    image=models.ImageField(default='default.png',upload_to='images/')
+    title=models.CharField(default='作品标题',max_length=50)
+
+    def __str__(self):
+        return self.title
+
