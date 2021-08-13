91f405f5829f95a9b3d7b0f1adaa9b780d261cad
金楠 <1299973527@qq.com>
2021-08-13 22:35:54 +0800

    '添加了每一条资讯的具体信息'

--- firstblog/urls.py
+++ firstblog/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('blog/', blog.views.blog_page),
+    path('blog/<int:blog_id>/', blog.views.blog_text),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
