dabdd570b25cb4b8f967f9873c58fc16b94aaa92
金楠 <1299973527@qq.com>
2021-08-13 19:15:04 +0800

    '美化了blog页面'

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,10 @@ class Blog(models.Model):
     title=models.CharField(default='新闻标题',max_length=50)
     date=models.DateField()
     image=models.ImageField(default='default.png',upload_to='images/')
-    text=models.CharField(default='新闻正文',max_length=500)
+    text=models.TextField(default='新闻正文')
 
     def __str__(self):
         return self.title
+
+    def short_text(self):
+        return self.text[:60]+'...'  #字符串切片，只显示文章前100个字
